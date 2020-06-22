ff675525857a396c68b51bfbaab07c43d36bc933
Mariannkmb <62664437+Mariannkmb@users.noreply.github.com>
2020-06-21 20:50:15 -0500

    API Openweather
    get values from API

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 import date from "date-and-time";
 import WeatherDetails from "./WeatherDetails";
@@ -7,9 +8,42 @@ import WeatherForecast from "./WeatherForecast";
 export default function Weather(props) {
   const now = new Date();
   const pattern = date.compile(" ddd, MMM DD YYYY");
+  const [data, setData] = useState({ submit: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function getData(response) {
+    setData({
+      submit: true,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      tempmax: Math.round(response.data.main.temp_max),
+      tempmin: Math.round(response.data.main.temp_min),
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity();
+  }
+
+  function handleCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function searchCity() {
+    let apiKey = "b5de5ed43000236f70d3412957f9f340";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(getData);
+  }
 
   let headerForm = (
-    <form className="form-inline search-box">
+    <form className="form-inline search-box" onSubmit={handleSubmit}>
       <input
         className="form-control mr-sm-2"
         type="search"
@@ -17,6 +51,7 @@ export default function Weather(props) {
         aria-label="Search"
         id="searchButton"
         autocomplete="off"
+        onChange={handleCity}
       />
       <button
         className="btn btn-outline-secondary my-2 my-sm-0"
@@ -38,12 +73,17 @@ export default function Weather(props) {
     </form>
   );
 
-  return (
-    <div className="WeatherAppWrapper">
-      <h1 className="AppTitle">Weather</h1>
-      <div className="row RowHeader">{headerForm}</div>
-      <WeatherDetails city={props.defaultCity} />
-      <WeatherForecast />
-    </div>
-  );
+  if (data.submit) {
+    return (
+      <div className="WeatherAppWrapper">
+        <h1 className="AppTitle">Weather</h1>
+        <div className="row RowHeader">{headerForm}</div>
+        <WeatherDetails data={data} />
+        <WeatherForecast />
+      </div>
+    );
+  } else {
+    searchCity();
+    return "Loading...";
+  }
 }
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -4,13 +4,18 @@ export default function WeatherDetails(props) {
   return (
     <div className="row RowTodayForecast">
       <div className="col-5">
-        <h2 className="City"> {props.city} </h2>
-        <p className="Main">Precipitation: Humidity % Wind : Wind km/hr</p>
+        <h2 className="City"> {props.data.city} </h2>
+        <p className="Main">
+          <ul>
+            <li> Precipitation: {props.data.humidity} % </li>
+            <li> Wind : {props.data.wind} km/hr </li>
+          </ul>
+        </p>
       </div>
       <div className="col-2 TempCeld">
         <p>
           <h2>
-            <span className="Temperature">20</span>
+            <span className="Temperature">{props.data.temperature}</span>
             <span className="Metric">˚C</span>
           </h2>
           <h5 className="change-metric">
@@ -25,12 +30,8 @@ export default function WeatherDetails(props) {
         </p>
       </div>
       <div className="col-5 WeatherImageCeld">
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-          alt=""
-          id="weather-icon"
-        />
-        <p className="WeatherDescrip">Cloudy</p>
+        <img src={props.data.icon} alt="" id="weather-icon" />
+        <p className="WeatherDescrip">{props.data.description}</p>
       </div>
     </div>
   );
