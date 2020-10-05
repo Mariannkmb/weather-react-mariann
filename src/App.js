004c051c904f2cb4fdf89308bd9ee262ec8c73d2
Mariannkmb <62664437+Mariannkmb@users.noreply.github.com>
2020-10-05 00:37:45 -0500

    Change Default City

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Lisbon" />
+        <Weather defaultCity="Lima" />
         <footer>
           <small id="gitHubUrl">
             <a
